badad3c0259406548335b6d0c69f84a26c111913
SukiBaki <sukibaki87@gmail.com>
2024-12-30 17:04:18 +0800

    Add files via upload
    The first version of GUI, implemented using IEDA's Java Swing based visual GUI design functionality

---
+++ VoiceChatSystemGUI/src/UI/MainInterface.java
@@ -0,0 +1,14 @@
+package UI;
+
+import javax.swing.*;
+
+public class MainInterface {
+    private JPanel mainPanel;
+    private JPanel chatInterface;
+    private JTextField textField1;
+    private JButton 发送Button;
+    private JButton 查看聊天记录Button;
+    private JTextField textField2;
+    private JButton 修改Button;
+    private JComboBox comboBox1;
+}
